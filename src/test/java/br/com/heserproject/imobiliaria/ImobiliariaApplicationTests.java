9fdc664f76542867af278b87db776084dce648b4
VitorHeser <54639202+VitorHeser@users.noreply.github.com>
2025-06-23 20:51:39 -0300

    first commit

---
+++ src/test/java/br/com/heserproject/imobiliaria/ImobiliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.heserproject.imobiliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImobiliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
