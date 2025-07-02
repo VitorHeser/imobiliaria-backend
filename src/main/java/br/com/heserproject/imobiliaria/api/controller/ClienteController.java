2768479b0160e5ccdc505df7726c1115d348b67d
EwertonSantos <ewerton.hsantos91@gmail.com>
2025-07-02 19:37:06 -0300

    Att Base controle do cliente.

--- src/main/java/br/com/heserproject/imobiliaria/api/controller/ClienteController.java
+++ src/main/java/br/com/heserproject/imobiliaria/api/controller/ClienteController.java
@@ -1,5 +1,7 @@
 package br.com.heserproject.imobiliaria.api.controller;
 
+import br.com.heserproject.imobiliaria.core.base.BaseApiResponse;
+import br.com.heserproject.imobiliaria.core.base.BaseSimpleController;
 import br.com.heserproject.imobiliaria.domain.entity.Cliente;
 import br.com.heserproject.imobiliaria.service.ClienteService;
 import lombok.RequiredArgsConstructor;
@@ -7,40 +9,39 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.springframework.util.MimeTypeUtils.APPLICATION_JSON_VALUE;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/cliente")
-public class ClienteController {
+public class ClienteController extends BaseSimpleController {
 
     private final ClienteService clienteService;
 
     @GetMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<List<Cliente>> findAll() {
-        return ResponseEntity.of(Optional.of(clienteService.findAll()));
+    public ResponseEntity<BaseApiResponse<List<Cliente>>> findAll() {
+        return sendResponseOK(clienteService::findAll);
     }
 
     @GetMapping(path = "/{id}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<Cliente> findById(@PathVariable Integer id) {
-        return ResponseEntity.of(Optional.of(clienteService.findById(id)));
+    public ResponseEntity<BaseApiResponse<Cliente>> findById(@PathVariable Integer id) {
+        return sendResponseOK(() -> clienteService.findById(id));
     }
 
     @DeleteMapping(path = "/{id}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public void deleteById(@PathVariable Integer id) {
-        clienteService.deleteById(id);
+    public ResponseEntity<BaseApiResponse<Void>> deleteById(@PathVariable Integer id) {
+        return sendResponseOK(() -> clienteService.deleteById(id));
     }
 
     @PutMapping(path = "/{id}", consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<Cliente> atualizar(@PathVariable Integer id,
-                                             @RequestBody Cliente cliente) {
-        return ResponseEntity.of(Optional.of(clienteService.atualizar(id, cliente)));
+    public ResponseEntity<BaseApiResponse<Cliente>> atualizar(@PathVariable Integer id,
+                                                              @RequestBody Cliente cliente) {
+        return sendResponseCreated(() -> clienteService.atualizar(id, cliente));
     }
 
     @PostMapping(consumes = APPLICATION_JSON_VALUE, produces = APPLICATION_JSON_VALUE)
-    public ResponseEntity<Cliente> salvar(@RequestBody Cliente cliente) {
-        return ResponseEntity.of(Optional.of(clienteService.salvar(cliente)));
+    public ResponseEntity<BaseApiResponse<Cliente>> salvar(@RequestBody Cliente cliente) {
+        return sendResponseCreated(() -> clienteService.salvar(cliente));
     }
 }
